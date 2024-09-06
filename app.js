b5dbff74c82dd30207d86f589a64e1b1cc52f025
doboy <doboy0@gmail.com>
2024-09-06 12:02:52 -0700

    use icons for header items

--- app.js
+++ app.js
@@ -167,8 +167,7 @@ const computeCalendar = (date, unreservedData, securedData, park) => {
 
 const Menu = ({ selectedMenuItem, setSelectedMenuItem }) => {
   return (
-    <div>
-      <div className="ui container" style={{paddingBottom: 50}}>
+      <div className="ui container">
         <div className="ui secondary pointing menu">
           <a href="#zitnr" className={classnames(["header", { active: selectedMenuItem == "#zitnr" || selectedMenuItem == "" }, "item"])} onClick={() => { setSelectedMenuItem("#zitnr") }}>
             z.i.t.n.r.
@@ -176,13 +175,16 @@ const Menu = ({ selectedMenuItem, setSelectedMenuItem }) => {
 
           <div className="right menu">
             <a href="#calendar" className={classnames(["header", { active: selectedMenuItem == "#calendar" }, "item"])} onClick={() => { setSelectedMenuItem("#calendar") }}>
-              calendar
+              <i className="calendar alternate icon"></i>
             </a>
             <a href="#reservations" className={classnames(["header", { active: selectedMenuItem == "#reservations" }, "item"])} onClick={() => { setSelectedMenuItem("#reservations") }}>
-              reservations
+              <i className="file alternate icon"></i>
             </a>
             <a href="#donate" className={classnames(["header", { active: selectedMenuItem == "#donate" }, "item"])} onClick={() => { setSelectedMenuItem("#donate") }}>
-              beer
+              <i className="money bill alternate icon"></i>
+            </a>
+            <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSd85TIFziQZHXxZm_9uQ4YDjJVCo4yyrhrvCESlu0ryS-ptZg/viewform?usp=sf_link" className={classnames(["header item"])}>
+              <i className="mail icon"></i>
             </a>
           </div>
         </div>
@@ -200,12 +202,6 @@ const Menu = ({ selectedMenuItem, setSelectedMenuItem }) => {
           }
         })()}
       </div>
-      <div className="ui center aligned basic segment" style={{ position: "fixed", backgroundColor: 'white', bottom: 0, width: "100%", borderTop: "2px solid rgba(34,36,38,.15)" }}>
-        <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSd85TIFziQZHXxZm_9uQ4YDjJVCo4yyrhrvCESlu0ryS-ptZg/viewform?usp=sf_link">
-          Feedback
-        </a>
-      </div>
-    </div>
   )
 }
 
