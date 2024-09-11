722df7d82b83501cdab3867e0e26a352a6a1f325
doboy <doboy0@gmail.com>
2024-09-11 13:40:22 -0700

    add rainier courts

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const BEACON_HILL_ID = "1319";
 const BITTER_LAKE_1_ID = "1315";
 const BITTER_LAKE_2_ID = "1316";
 const BITTER_LAKE_3_ID = "1317";
+const RAINIER_BEACH_1_ID = "1379";
+const RAINIER_BEACH_2_ID = "1380";
+const RAINIER_BEACH_3_ID = "1381";
+const RAINIER_1_ID = "1383";
+const RAINIER_2_ID = "1384";
+const RAINIER_3_ID = "1385";
 
 const START_TIME_BY_PARK_ID = {
   [MILLER_PARK_ID]: "7:00 AM",
@@ -13,6 +19,12 @@ const START_TIME_BY_PARK_ID = {
   [BITTER_LAKE_1_ID]: "7:00 AM",
   [BITTER_LAKE_2_ID]: "7:00 AM",
   [BITTER_LAKE_3_ID]: "7:00 AM",
+  [RAINIER_BEACH_1_ID]: "8:30 AM",
+  [RAINIER_BEACH_2_ID]: "8:30 AM",
+  [RAINIER_BEACH_3_ID]: "8:30 AM",
+  [RAINIER_1_ID]: "7:00 AM",
+  [RAINIER_2_ID]: "7:00 AM",
+  [RAINIER_3_ID]: "7:00 AM",
 };
 
 const PARKS = [{
@@ -20,7 +32,7 @@ const PARKS = [{
   name: "Miller Park",
 }, {
   id: BAKER_PARK_ID,
-  name: "Baker Park",
+  name: "Mt. Baker Park",
 }, {
   id: BEACON_HILL_ID,
   name: "Beacon Hill Park",
@@ -33,6 +45,24 @@ const PARKS = [{
 }, {
   id: BITTER_LAKE_3_ID,
   name: "Bitter Lake Court 3",
+}, {
+  id: RAINIER_BEACH_1_ID,
+  name: "Rainier Beach Court 1",
+}, {
+  id: RAINIER_BEACH_2_ID,
+  name: "Rainier Beach Court 2",
+}, {
+  id: RAINIER_BEACH_3_ID,
+  name: "Rainier Beach Court 3",
+}, {
+  id: RAINIER_1_ID,
+  name: "Rainier Court 1",
+}, {
+  id: RAINIER_2_ID,
+  name: "Rainier Court 2",
+}, {
+  id: RAINIER_3_ID,
+  name: "Rainier Court 3",
 }]
 
 const updateQueryStringParameter = (key, val) => {
