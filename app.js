398b87662963ed2b157486e8dc4d63ffbc8e18b6
doboy <doboy0@gmail.com>
2024-09-13 13:24:01 -0700

    make it clear that zitnr is non-profit

--- app.js
+++ app.js
@@ -230,7 +230,7 @@ const Menu = ({ selectedMenuItem, setSelectedMenuItem }) => {
 const ZitnrTab = () => {
   return <div className="ui basic segment">
     <span>
-      Welcome to z.i.t.n.r.! <br /><br />Our project aims to enhance your pickleball experience at Miller Park in Seattle. We do this by reserving courts for open play during peak hours and providing reservations information. If you find this service helpful, please consider supporting us with a donation so we can continue to secure court access for everyone.
+      Welcome to z.i.t.n.r.! <br /><br />Our non-profit project aims to enhance your pickleball experience at Miller Park in Seattle. We do this by reserving courts for open play during peak hours and providing reservations information. If you find this service helpful, please consider supporting us with a donation so we can continue to secure court access for everyone.
     </span>
   </div>
 };
