cd95f2f8141e4b92c471fc3c945efd15d43c4225
Huan (Zack) Do <doboy0@gmail.com>
2025-07-11 10:18:58 -0700

    remove unused constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,2 @@
-export const MILLER_PARK_COURT_ID = 1374;
-export const BAKER_PARK_COURT_ID = 1378;
 export const COURT_STATUS_DATABASE_NAME =
   window.location.hostname == "zitnr.com" ? "courtStatus" : "courtStatusDev";
